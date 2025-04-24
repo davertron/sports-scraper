f5dce0ef041fc7fac7856639492b82fed631f4c4
davertron <davertron@users.noreply.github.com>
2025-04-24 03:21:01 +0000

    deploy: 98667d995ae2547cc6b5336c6b2488246e204284

--- schedule.js
+++ schedule.js
@@ -3,5 +3,5 @@
     .then(response => response.json())
     .then(data => {
       const scheduleDiv = document.getElementById('schedule');
-      scheduleDiv.innerHTML = data.map(game => `<p>${game.date}: ${game.team}</p>`).join('');
+      scheduleDiv.innerHTML = data.map(game => `<p>${game.rink}: ${game.title}</p>`).join('');
     });
