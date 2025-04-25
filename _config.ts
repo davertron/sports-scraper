76632203312bbeb11bf2bf5bd60ce7a5deccc68a
Dave Davis <dabukun@gmail.com>
2025-04-24 23:24:02 -0400

    Disable pretty URLs for compatibility with GitHub Pages

--- _config.ts
+++ _config.ts
@@ -2,6 +2,7 @@ import lume from "lume/mod.ts";
 
 const site = lume({
   location: new URL("https://davertron.github.io/sports-scraper/"),
+  prettyUrls: false, // These don't work on GitHub Pages
 });
 
 site.copy("static");
