9a2a8040161fb6d7182ad18daa39c99c937abb55
Dave Davis <dabukun@gmail.com>
2025-04-25 21:22:41 -0400

    Refactor Lume configuration to remove hardcoded site URL and delete obsolete GitHub Actions workflow for deployment

--- _config.ts
+++ _config.ts
@@ -1,10 +1,7 @@
 import lume from "lume/mod.ts";
 import basePath from "lume/plugins/base_path.ts";
 
-const site = lume({
-  location: new URL("https://davertron.github.io/sports-scraper/"),
-  prettyUrls: false, // These don't work on GitHub Pages
-});
+const site = lume();
 
 site.copy("static");
 site.use(basePath());
