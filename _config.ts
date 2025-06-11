75ad5ce5986b511ce88474b81ff56f964306fa00
Dave Davis <dabukun@gmail.com>
2025-06-11 05:49:39 -0400

    Add tailwind

--- _config.ts
+++ _config.ts
@@ -1,6 +1,14 @@
 import lume from "lume/mod.ts";
+import tailwindcss from "lume/plugins/tailwindcss.ts";
+import postcss from "lume/plugins/postcss.ts";
 
 const site = lume();
+
+// Add PostCSS plugin
+site.use(tailwindcss());
+site.use(postcss());
+
+site.add("static/styles.css");
 
 site.copy("static");
 site.ignore("frontend");
